3b53ca1b385a6b884d73c705160c4eceb1e4556e
alexanguianog <alex1203@live.com.mx>
2018-11-29 02:28:35 -0700

    Update requestoutfit.php

--- requestoutfit.php
+++ requestoutfit.php
@@ -14,37 +14,15 @@ $dbname = "latmyodb";
 //Connection to databse
 $conn = new mysqli ($host, $dbUsername, $dbPassword, $dbname);
 
-    //create connection
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-
-    if (mysqli_connect_error()) {
-         die('Connect Error('. mysqli_connect_error().')'. mysqli_connect_error());
-        } else {
-         $SELECT = "SELECT outfitname From requestoutfit Where outfitname = ? Limit 1";
-         $INSERT = "INSERT Into requestoutfit (user, outfitname, season, time, reason, comments) values(?, ?, ?, ?, ?, ?)";
-         //Prepare statement
-         $stmt = $conn->prepare($SELECT);
-         $stmt->bind_param("s", $outfitname);
-         $stmt->execute();
-         $stmt->bind_result($outfitname);
-         $stmt->store_result();
-         $rnum = $stmt->num_rows;
-         if ($rnum==0) {
-          $stmt->close();
-          $stmt = $conn->prepare($INSERT);
-          $stmt->bind_param("ssssss", $user, $outfitname, $season, $time, $reason, $comments);
-          $stmt->execute();
-          echo "Your request has been sent to one of our fashion experts sucessfully";
-         } else {
-          die();
-         }
-         $stmt->close();
-         $conn->close();
-        }
-    } else {
-     echo "All field are required";
-     die();
-    }
-    ?>
+if (mysqli_connect_error()) {
+  die('Connect Error('. mysqli_connect_error().')'. mysqli_connect_error());
+} else {
+  //Values insertion to database with respective tables
+  $sql = "INSERT INTO requestoutfit (user, outfitname, season, time, reason, comments) values('$user','$outfitname','$season','$time','$reason','$comments')";
+  //Validation on screen
+  echo "<img style='width:500px;margin-top:50px;margin-bottom:50px;' src='images/LookatmyOutfitLarge.png'><br><h2><font color='#E27C7C' style='font-size:30px'>Your request has been sent to one of our fashion experts sucessfully, you will get a notification with our recommendation in some minutes.<br><br><a href='requestoutfit.html'>Return</a></font></h2>";
+  $conn->query($sql);
+  //Close connection to databse
+  $conn->close();
 }
 ?>
